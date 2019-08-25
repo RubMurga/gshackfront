a4b51d47291a077e2e5eaf6a6e9dcddf70855715
Rubén Murga <ruben.murga.d@gmail.com>
2019-08-25 11:08:06 -0500

    changing input

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCGzSAUJXpkHiw2KbVbYbpD3HfpDKE2yGs",
+    authDomain: "ayudala-da5a4.firebaseapp.com",
+    databaseURL: "https://ayudala-da5a4.firebaseio.com",
+    projectId: "ayudala-da5a4",
+    storageBucket: "ayudala-da5a4.appspot.com",
+    messagingSenderId: "476785606259",
+    appId: "1:476785606259:web:2a5d7deae65e0685"
+  }
 };
